11c8e8ab2519808e2c0b6f1d4fcc8e26bd16c2b8
Sbilavanh <bilavanh.souphalak@gmail.com>
2022-08-19 14:36:16 -0700

    update

--- crud.js
+++ crud.js
@@ -3,6 +3,10 @@ class House {
         this.name = name;
         this.rooms = [];
     }
+
+    addRoom(name, area) {
+        this.rooms.push(new Room(name, area));
+    }
 }
 
 class Room {
@@ -12,7 +16,7 @@ class Room {
     }
 }
 
-class HouseServicve {
+class HouseService {
     static url = 'https://ancient-taiga-31359.herokuapp.com/api/houses';
 
     static getAllHouses() {
@@ -36,4 +40,37 @@ class HouseServicve {
             type: 'PUT'
         });
     }
+
+    static deleteHouse(id) {
+        return $.ajax({
+            url: this.url + `/${id}`,
+            type: 'DELETE'
+        });
+    }
 }
+
+class DOMManager {
+    static houses;
+
+    static getAllHouses() {
+        HouseService.getAllHouses().then(houses => this.render(houses));
+    }
+
+    static render(houses) {
+        this.houses = houses;
+        $('#app').empty();
+        for (let house of houses) {
+            $('#app').prepend(
+                `<div id="${house._id}" class="card">
+                    <div class="card-header">
+                        <h2>${house.name}</h2>
+                    </div>
+                </div>
+                `
+            );
+        }
+    }
+
+}
+
+DOMManager.getAllHouses
