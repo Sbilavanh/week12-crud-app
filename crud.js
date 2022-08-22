ed689ac5e5f2937316da58d008370848e6e292e9
Sbilavanh <bilavanh.souphalak@gmail.com>
2022-08-21 23:24:35 -0700

    update

--- crud.js
+++ crud.js
@@ -15,53 +15,52 @@ function onFormSubmit(e) {
 
 function readFormData() {
     var formData = {};
-    formData["Vehicles"] = document.getElementById("Vehicles").value;
-    formData["Course"] = document.getElementById("Course").value;
-    formData["Class"] = document.getElementById("Class").value;
+    formData["vehicle"] = document.getElementById("vehicle").value;
+    formData["course"] = document.getElementById("course").value;
+    formData["class"] = document.getElementById("class").value;
     return formData;
 }
 
 
 function insertNewRecord(data) {
-    var table = document.getElementById("menuSelect").getElementsByTagName('tbody')[0];
+    var table = document.getElementById("vehicleSelect").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
     cell1 = newRow.insertCell(0);
-    cell1.innerHTML = data.Vehicles;
+    cell1.innerHTML = data.vehicle;
     cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.Course;
+    cell2.innerHTML = data.course;
     cell3 = newRow.insertCell(2);
-    cell3.innerHTML = data.Class;
-    cell4 = newRow.insertCell(4);
+    cell3.innerHTML = data.class;
+    cell4 = newRow.insertCell(3);
     cell4.innerHTML = `<button onClick="onEdit(this)">Edit</button> <button onClick="onDelete(this)">Delete</button>`;
 }
 
 
 function onEdit(td) {
     selectedRow = td.parentElement.parentElement;
-    document.getElementById("Vehicle").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("Course").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("Class").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("vehicle").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("course").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("class").value = selectedRow.cells[2].innerHTML;
 }
 function updateRecord(formData) {
-    selectedRow.cells[0].innerHTML = formData.Vehicles;
-    selectedRow.cells[1].innerHTML = formData.Course;
-    selectedRow.cells[2].innerHTML = formData.Class;
+    selectedRow.cells[0].innerHTML = formData.vehicle;
+    selectedRow.cells[1].innerHTML = formData.course;
+    selectedRow.cells[2].innerHTML = formData.class;
 }
 
 
 function onDelete(td) {
     if (confirm('Do you want to remove this?')) {
         row = td.parentElement.parentElement;
-        document.getElementById('menuSelect').deleteRow(row.rowIndex);
+        document.getElementById('vehicleSelect').deleteRow(row.rowIndex);
         resetForm();
     }
 }
 
 
 function resetForm() {
-    document.getElementById("Vehicle").value = '';
-    document.getElementById("Course").value = '';
-    document.getElementById("Class").value = '';
+    document.getElementById("vehicle").value = '';
+    document.getElementById("course").value = '';
+    document.getElementById("class").value = '';
     selectedRow = null;
 }
-
